9baf07431e08baccac85d8b7f3ebb82410cf7c48
PedroParraPerez <pedroparra06091994@gmail.com>
2022-04-04 12:25:38 +0000

    working css

--- src/js/component/MusicPlayer.jsx
+++ src/js/component/MusicPlayer.jsx
@@ -7,6 +7,7 @@ import rigoImage from "../../img/rigo-baby.jpg";
 const MusicPlayer = () => {
 	const [songList, setSongList] = useState("");
 	const [urlsong, setUrlsong] = useState("");
+	const [namesong, setNamesong] = useState("");
 
 	useEffect(() => {
 		TakeSongList();
@@ -30,31 +31,50 @@ const MusicPlayer = () => {
 
 	return (
 		<>
-			<h1>Music Player</h1>
+			{" "}
+			<div className="d-flex m-0 mt-5 justify-content-center  wrap">
+				<div className="d-block">
+					<h1 className="title text-center">Music Player</h1>
 
-			<ul>
-				{songList
-					? songList.map((song, i) => {
-							return (
-								<li
-									key={i}
-									onClick={() => {
-										setUrlsong(
-											"https://assets.breatheco.de/apis/sound/" +
-												song.url
-										);
-										console.log(urlsong);
-									}}>
-									{song.name}
-								</li>
-							);
-					  })
-					: ""}
-			</ul>
-			<audio controls autoPlay src={urlsong}>
-				Your browser does not support the
-				<code>audio</code> element.
-			</audio>
+					<ul className="p-0">
+						{songList
+							? songList.map((song, i) => {
+									return (
+										<li
+											className="song d-flex justify-content-between"
+											key={i}>
+											{song.name}
+											<span
+												className="song_play_button"
+												onClick={() => {
+													setUrlsong(
+														"https://assets.breatheco.de/apis/sound/" +
+															song.url
+													);
+													setNamesong(song.name);
+													console.log(urlsong);
+												}}>
+												Reproducir
+											</span>
+										</li>
+									);
+							  })
+							: ""}
+					</ul>
+					<div className="song_playing">
+						<span>Esta sonando: </span>
+						<span>{namesong ? namesong : ""}</span>
+					</div>
+					<audio
+						className="audiocontrols mt-3"
+						controls
+						autoPlay
+						src={urlsong}>
+						Your browser does not support the
+						<code>audio</code> element.
+					</audio>
+				</div>
+			</div>
 		</>
 	);
 };
